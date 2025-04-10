e1adac2996a5a92a87119c6607f84b0bb2773c1b
Mitchel Klijn <mitchelklijn2004@gmail.com>
2025-04-10 11:56:13 +0200

    <Feature> [Menu UI]: Added widget switching functionality and setup the basics in the main scene

--- Slagschip/Assets/Scripts/Multiplayer/MenuHandler.cs
+++ Slagschip/Assets/Scripts/Multiplayer/MenuHandler.cs
@@ -24,11 +24,21 @@ public class MenuHandler : MonoBehaviour
     {
         _document = GetComponent<UIDocument>();
 
+        _document.rootVisualElement.Query("make-session-button").First().RegisterCallback<ClickEvent>(OnMakeSession);
         _document.rootVisualElement.Query("quick-play-button").First().RegisterCallback<ClickEvent>(OnQuickPlay);
         _document.rootVisualElement.Query("play-code-input").First().RegisterCallback<InputEvent>(OnPlayCodeInputChanged);
         _document.rootVisualElement.Query("play-code-button").First().RegisterCallback<ClickEvent>(OnPlayCodeInputSubmitted);
+        _document.rootVisualElement.Query("credits-button").First().RegisterCallback<ClickEvent>(OnCredits);
         _document.rootVisualElement.Query("quit-button").First().RegisterCallback<ClickEvent>(OnQuit);
         _document.rootVisualElement.Query("start-session-button").First().RegisterCallback<ClickEvent>(OnStartSession);
+        _document.rootVisualElement.Query("cancel-button").First().RegisterCallback<ClickEvent>(OnCancel);
+        _document.rootVisualElement.Query("back-button").First().RegisterCallback<ClickEvent>(OnBack);
+    }
+
+    private void OnMakeSession(ClickEvent _event)
+    {
+        _document.rootVisualElement.Query("start-screen").First().style.display = DisplayStyle.None;
+        _document.rootVisualElement.Query("settings-screen").First().style.display = DisplayStyle.Flex;
     }
 
     private void OnQuickPlay(ClickEvent _event)
@@ -52,6 +62,12 @@ public class MenuHandler : MonoBehaviour
         playCodeButton.onClick.Invoke();
     }
 
+    private void OnCredits(ClickEvent _event)
+    {
+        _document.rootVisualElement.Query("start-screen").First().style.display = DisplayStyle.None;
+        _document.rootVisualElement.Query("credits-screen").First().style.display = DisplayStyle.Flex;
+    }
+
     private void OnQuit(ClickEvent _event)
     {
         Application.Quit();
@@ -64,5 +80,17 @@ public class MenuHandler : MonoBehaviour
         sceneLoader.LoadScene(loadingScene.name, false);
 
         startSessionButton.onClick.Invoke();
+    }
+
+    private void OnCancel(ClickEvent _event)
+    {
+        _document.rootVisualElement.Query("settings-screen").First().style.display = DisplayStyle.None;
+        _document.rootVisualElement.Query("start-screen").First().style.display = DisplayStyle.Flex;
+    }
+
+    private void OnBack(ClickEvent _event)
+    {
+        _document.rootVisualElement.Query("credits-screen").First().style.display = DisplayStyle.None;
+        _document.rootVisualElement.Query("start-screen").First().style.display = DisplayStyle.Flex;
     }
 }
